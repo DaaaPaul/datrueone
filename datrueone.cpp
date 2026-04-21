4aa6e8b582232a3c2fb1465f21ef7b78362dfa89
DaaaPaul <paulpeng2008@gmail.com>
2026-04-21 14:51:40 -0400

    add early exit option with handling

--- datrueone.cpp
+++ datrueone.cpp
@@ -5,15 +5,16 @@
 #include <string>
 #include <cmath>
 #include <random>
+#include <csignal>
 #include <vector>
 #include <fstream>
 
-struct Time {
+struct Clock {
 	uint32_t minutes{};
 	uint32_t seconds{};
 
-	static Time decrement(Time const& T) noexcept {
-		Time t = T;
+	static Clock decrement(Clock const& T) noexcept {
+		Clock t = T;
 
 		if(t.seconds > 0) {
 			t.seconds--;
@@ -25,7 +26,7 @@ struct Time {
 		return t;
 	}
 
-	static std::string toString(uint32_t const& MS) {
+	static std::string digitsString(uint32_t const& MS) {
 		std::string str{};
 		if(MS < 10) {
 			str = std::string("0") + std::to_string(MS);
@@ -34,19 +35,17 @@ struct Time {
 		}
 		return str;
 	}
-	friend std::ostream& operator<<(std::ostream& os, const Time& time);
+
+	static std::string toString(Clock const& T) {
+		return Clock::digitsString(T.minutes) + ":" + Clock::digitsString(T.seconds);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Clock& gClock);
 };
 
-std::ostream& operator<<(std::ostream& os, const Time& time) {
-	os << Time::toString(time.minutes) << ":" << Time::toString(time.seconds);
+std::ostream& operator<<(std::ostream& os, const Clock& gClock) {
+	os << Clock::toString(gClock);
     return os;
-}
-
-double random() noexcept {
-	static std::default_random_engine gEngine(static_cast<unsigned>(time(nullptr)));
-	static std::uniform_real_distribution gRange(0.0f, 1.0f);
-
-	return gRange(gEngine);
 }
 
 void putFile(const std::string& name, const std::string& content, bool append = true) {
@@ -58,7 +57,14 @@ void putFile(const std::string& name, const std::string& content, bool append = 
     outfile << content;
 }
 
-std::vector<std::pair<std::string, std::string>> msgs{
+float random() {
+	static std::default_random_engine gEngine(static_cast<unsigned>(time(nullptr)));
+	static std::uniform_real_distribution gRange(0.0f, 1.0f);
+
+	return gRange(gEngine);
+}
+
+std::vector<std::pair<std::string, std::string>> endMsgs{
 	{"Hey chud! Did you ", "?"},
 	{"Don't worry, you're only going to hell if you didn't ", "."},
 	{"You're going to heaven actually. But you did ", " right?"},
@@ -68,13 +74,33 @@ std::vector<std::pair<std::string, std::string>> msgs{
 	{"Just did it (or not): ", ""}
 };
 
+std::vector<std::string> earlyMsgs{
+	"You know what they say... Early bird gets the worm!",
+	"Godspeed!",
+	"Yeah I'm going hard today cuz for me you would be a calllll away!!",
+	"Tsssk. Not enough. You're better.",
+};
+
+const std::string TALLY_PATH = R"(C:\Users\paulp\datrueoneFiles\Tally.txt)";
+const std::string START_PATH = R"(C:\Users\paulp\datrueoneFiles\DUCKSWORTH.mp3)";
+const std::string FINISH_PATH = R"(C:\Users\paulp\datrueoneFiles\Tolle Bell.mp3)";
+const std::string EARLY_FINISH_PATH = R"(C:\Users\paulp\datrueoneFiles\GHOSTEMANE.mp3)";
+
+Clock gClock{};
+std::string gGoal{};
+std::string gGiven{};
+bool gQuit = false;
+
+void ctrlC(int x) {
+	gQuit = true;
+}
+
 int main() {
 	ma_engine engine;
 	ma_engine_init(nullptr, &engine);
 
-	std::string goal{};
 	std::cout << "GOAL: ";
-	std::getline(std::cin, goal);
+	std::getline(std::cin, gGoal);
 
 	double minutes{};
 	std::cout << "MINUTES: ";
@@ -82,24 +108,41 @@ int main() {
 
 	double pureMinutes{};
 	double decimal = std::modf(minutes, &pureMinutes);
+	gClock.minutes = pureMinutes;
+	gClock.seconds = decimal * 60.0;
+	gGiven = Clock::toString(gClock);
 
-	Time timeLeft{pureMinutes, decimal * 60.0};
-	std::string initialTimeLeft = Time::toString(timeLeft.minutes) + ":" + Time::toString(timeLeft.seconds);
+	ma_engine_play_sound(&engine, START_PATH.c_str(), nullptr);
+	signal(SIGINT, ctrlC);
 
-	ma_engine_play_sound(&engine, R"(C:\Users\paulp\datrueoneFiles\DUCKSWORTH.mp3)", nullptr);
-		
-	while(timeLeft.minutes > 0 || timeLeft.seconds > 0) {
-		std::cout << timeLeft << "\n";
-		timeLeft = Time::decrement(timeLeft);
+	while ((!gQuit) && ((gClock.minutes > 0) || (gClock.seconds > 0))) {
+		std::cout << gClock << "\n";
+		gClock = Clock::decrement(gClock);
 		Sleep(1000);
 	}
-	std::cout << timeLeft << "\n";
+	std::cout << gClock << "\n";
 
-	ma_engine_play_sound(&engine, R"(C:\Users\paulp\datrueoneFiles\Tolle Bell.mp3)", nullptr);
-	std::pair<std::string, std::string> msg = msgs[random() * 7.0];
-	std::cout << msg.first + goal + msg.second + " ";
-	std::string result{};
-	std::getline(std::cin, result);
+	if(gQuit) {
+		ma_engine_play_sound(&engine, EARLY_FINISH_PATH.c_str(), nullptr);
+		std::string msg = earlyMsgs[random() * 4.0];
 
-	putFile(R"(C:\Users\paulp\datrueoneFiles\Tally.txt)", msg.first + goal + msg.second + " " + result + " (" + initialTimeLeft + ")\n");
+		std::cout << msg << " ";
+
+		std::string input{};
+		std::getline(std::cin, input);
+		std::getline(std::cin, input);
+
+		std::string early = Clock::toString(gClock);
+
+		putFile(TALLY_PATH, gGoal + ":" + input + " (" + gGiven + " stopped at " + early + ")\n");
+	} else {
+		ma_engine_play_sound(&engine, FINISH_PATH.c_str(), nullptr);
+		std::pair<std::string, std::string> msg = endMsgs[random() * 7.0];
+		std::cout << msg.first + gGoal + msg.second + " ";
+		std::string input{};
+		std::getline(std::cin, input);
+		std::getline(std::cin, input);
+
+		putFile(TALLY_PATH, gGoal + ":" + input + " (" + gGiven + ")\n");
+	}
 }
